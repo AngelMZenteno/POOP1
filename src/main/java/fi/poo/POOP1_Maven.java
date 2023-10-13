b33235f41a8f20958fe929a86d3bdcd11896f38a
AngelMZenteno <143782752+AngelMZenteno@users.noreply.github.com>
2023-10-13 05:48:12 -0600

    Creación de practica

---
+++ src/main/java/fi/poo/POOP1_Maven.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package fi.poo;
+
+/**
+ *
+ * @author Zenteno Loyola Angel Maurice
+ */
+public class POOP1_Maven {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo desde POO grupo 8 con Maven");
+    }
+}
